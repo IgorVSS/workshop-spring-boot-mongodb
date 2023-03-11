1cb88ff3c0f7a8f15fda502e4cf823dc57503f38
Igor <bealmont@gmail.com>
2023-03-11 18:17:56 -0300

    Obtendo um usuario por id

--- src/main/java/com/bealmont/workshopmongo/services/UserService.java
+++ src/main/java/com/bealmont/workshopmongo/services/UserService.java
@@ -1,21 +1,27 @@
 package com.bealmont.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bealmont.workshopmongo.domain.User;
 import com.bealmont.workshopmongo.repository.UserRepository;
+import com.bealmont.workshopmongo.services.exception.ObjectNotFoundException;
 
-@Service	
+@Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
